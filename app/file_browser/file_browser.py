f80380d89983047f22aed3cccb7ccf3e4a43024e
Sych Sergiy <sychsergiy@gmail.com>
2017-12-27 14:00:03 +0200

    file browser exception handlers added, kivy import changed on factory-register

--- app/file_browser/file_browser.py
+++ app/file_browser/file_browser.py
@@ -1,6 +1,7 @@
 import os
 
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 
 from helper import DataHelper
@@ -25,16 +26,29 @@ class FileBrowser(FloatLayout):
         self._popup.open()
 
     def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-            data_helper = DataHelper(stream.read())
-            self.scroll_view.set_data_helper(data_helper)
-        self.dismiss_popup()
+        try:
+            with open(os.path.join(path, filename[0])) as stream:
+                data_helper = DataHelper(stream.read())
+                self.scroll_view.set_data_helper(data_helper)
+            self.dismiss_popup()
+        except:
+            self.error_popup('Something went wrong.\nCan not load selected file')
 
     def save(self, path, filename):
-        with open(os.path.join(path, filename), 'w') as stream:
-            for row in self.scroll_view.data_helper.data:
-                for cell in row:
-                    stream.write('{}\t'.format(cell))
+        try:
+            with open(os.path.join(path, filename), 'w') as stream:
+                for row in self.scroll_view.data_helper.data:
+                    for cell in row:
+                        stream.write('{}\t'.format(cell))
+                    stream.write('\n')
                 stream.write('\n')
-            stream.write('\n')
-        self.dismiss_popup()
+            self.dismiss_popup()
+        except:
+            self.error_popup('Something went wrong.\nCan not save file')
+
+    @staticmethod
+    def error_popup(message):
+        error_label = Label(text=message)
+        popup = Popup(title="Load file", content=error_label,
+                      size_hint=(0.4, 0.4))
+        popup.open()
--- app/main.py
+++ app/main.py
@@ -2,11 +2,18 @@ import kivy
 
 from kivy.app import App
 from kivy.base import Builder
+from kivy.factory import Factory
+
+from file_browser.file_browser import FileBrowser
+from table.scroll_view import TableScrollView
 
 kivy.require('1.10.0')
 
 Builder.load_file('file_browser/file_browser.kv')
 Builder.load_file('table/table.kv')
+
+Factory.register('FileBrowser', cls=FileBrowser)
+Factory.register('TableScrollView', cls=TableScrollView)
 
 
 class TableEditorApp(App):
